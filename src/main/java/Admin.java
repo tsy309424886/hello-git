e1b10da4cd0621d6c33b1261702d52a0c5a1cc47
tsy <309424886@qq.com>
2018-07-11 16:19:34 +0800

    hello-git第一次提交

---
+++ src/main/java/Admin.java
@@ -0,0 +1,14 @@
+/**
+ * Created by lx on 2018/7/11.
+ */
+public class Admin {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/User.java
@@ -0,0 +1,14 @@
+/**
+ * Created by lx on 2018/7/11.
+ */
+public class User {
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
