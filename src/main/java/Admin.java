0ef9b47faf962571f31533540b4060fc479a7565
tsy <309424886@qq.com>
2018-07-11 16:28:19 +0800

    下载第1次提交

--- src/main/java/Admin.java
+++ src/main/java/Admin.java
@@ -3,6 +3,15 @@
  */
 public class Admin {
     private String name;
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public String getName() {
         return name;
